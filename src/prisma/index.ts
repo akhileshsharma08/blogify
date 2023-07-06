c57d5514f2bc7d863367c64422fba06fcc4c6873
Akhilesh Sharma <58417806+akhileshsharma08@users.noreply.github.com>
2023-07-06 15:47:04 +0530

    prisma error fixed

--- src/prisma/index.ts
+++ src/prisma/index.ts
@@ -1,13 +1,31 @@
-import { PrismaClient } from "@prisma/client";
+// import { PrismaClient } from "@prisma/client";
 
-let prisma: PrismaClient;
+// let prisma: PrismaClient;
+// declare global {
+//   namespace NodeJS {
+//     interface Global {
+//       prisma: PrismaClient;
+//     }
+//   }
+// }
+
+// if (process.env.NODE_ENV === "production") {
+//   prisma = new PrismaClient();
+// } else {
+//   if (!global.prisma) {
+//     global.prisma = new PrismaClient();
+//   }
+//   prisma = global.prisma;
+// }
+
+// export default prisma;
+
 declare global {
-  namespace NodeJS {
-    interface Global {
-      prisma: PrismaClient;
-    }
-  }
+  var prisma: PrismaClient; // This must be a `var` and not a `let / const`
 }
+
+import { PrismaClient } from "@prisma/client";
+let prisma: PrismaClient;
 
 if (process.env.NODE_ENV === "production") {
   prisma = new PrismaClient();
